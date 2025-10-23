0311ec1199cde84882fbe78a6ad58cdcfc0911fb
arshad-dev57 <qureshiarshad947@gmail.com>
2025-10-23 01:13:47 -0700

    new commit

--- server.js
+++ server.js
@@ -36,7 +36,3 @@ mongoose
 
 // ✅ Export app for Vercel
 module.exports = app;
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
